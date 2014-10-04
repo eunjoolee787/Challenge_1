c524b438209183ca1ecc7f327c4d04e0b9d6fce2
mrbarbasa <mrbarbasa@gmail.com>
2014-10-04 09:43:07 -1000

    Answer Challenge_1

--- challenge_1.js
+++ challenge_1.js
@@ -1,6 +1,10 @@
 exports.sumOfAMultiple = function(n){
   var sum = 0;
   // do your work here
-  
+  for (var x = 3; x < n; x++) {
+    if (x % 3 === 0 || x % 5 === 0) {
+      sum += x;
+    }
+  }
   return sum;
 };
